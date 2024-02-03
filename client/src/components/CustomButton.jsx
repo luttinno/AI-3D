4c941818412df4bb79d927bafb3fa327e26b650b
luttinno <luka.grdzelishvili1994@gmail.com>
2024-02-03 22:57:23 +0400

    first commit

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import state from "../store";
+import { useSnapshot } from "valtio";
+
+export const CustomButton = ({ type, tittle, customStyles, handleClick }) => {
+  const snap = useSnapshot(state);
+  const generateStyle = (type) => {
+    if (type === "filled") {
+      return {
+        backgroundColor: snap.color,
+        color: "#fff",
+      };
+    }
+  };
+
+  return (
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
+      {tittle}
+    </button>
+  );
+};
