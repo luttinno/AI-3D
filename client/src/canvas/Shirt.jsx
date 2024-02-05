c6b8ece8c23211409a3004b756982cf9afdd2023
luttinno <luka.grdzelishvili1994@gmail.com>
2024-02-05 20:08:41 +0400

    add AI

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -13,9 +13,6 @@ const Shirt = () => {
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
-  logoTexture.anisotropy = 16;
-  fullTexture.anisotropy = 16;
-
   useFrame((state, delta) =>
     easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
   );
@@ -46,6 +43,7 @@ const Shirt = () => {
             rotation={[0, 0, 0]}
             scale={0.15}
             map={logoTexture}
+            anisotropy={16}
             depthTest={false}
             depthWrite={true}
           />
